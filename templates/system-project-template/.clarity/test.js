719408ea705801b8ed35bcd0dab97879708bcff2
Jared Barnes <jared-barnes@leavitt.com>
2017-09-25 14:49:41 -0600

    Updated the test runner to be more readable.

--- templates/system-project-template/.clarity/test.js
+++ templates/system-project-template/.clarity/test.js
@@ -1,6 +1,9 @@
 var fs = require("fs-extra");
 var path = require("path");
 
+
+const seperator = "============================================================";
+const startSeperator = "************************************************************";
 const testDirectory = path.join(__dirname, "../lib/tests");
 const specialTestNames = {
     "prepare": true,
@@ -23,72 +26,90 @@ const promisify = (fn) => {
     }
 }
 
-fs.readdir(testDirectory).catch(() => {
-    console.log(`Couldn't find test folder here: ${testDirectory}`);
-    return [];
-}).then((files) => {
+fs.readdir(testDirectory).then((files) => {
     return files.reduce((promise, file) => {
 
         return promise.then(() => {
             if (path.extname(file) === ".js") {
+                console.log(startSeperator);
+                console.log(file);
+                console.log(startSeperator);
+                try {
+                    var tests = require(path.join(testDirectory, file));
+                } catch (error) {
+                    console.log("REQUIRE ERROR: " + file);
+                    console.log(seperator);
+                    console.log(error);
+                    console.log(seperator);
 
-                var tests = require(path.join(testDirectory, file));
+                    return Promise.resolve();
+                }
+
                 var defaultModules = tests.default || tests;
-
                 var prepare = promisify(defaultModules.prepare);
                 var destroy = promisify(defaultModules.destroy);
                 var clean = promisify(defaultModules.clean);
+
+                var preparePromise = prepare().catch((error) => {
+                    console.log("UNEXPECTED FAILURE ON PREPARE: " + file);
+                    console.log(seperator);
+                    console.log(error);
+                    console.log(seperator);
+
+                    return Promise.reject();
+                });
 
                 return Object.keys(defaultModules).filter((testName) => {
                     return specialTestNames[testName] == null;
                 }).reduce((promise, testName) => {
 
                     if (typeof defaultModules[testName] !== "function") {
-                        return Promise.resolve("UNEXPECTED EXPORT:  This export '" + testName + "' needs to be a function.");
+                        console.log("UNEXPECTED EXPORT:  This export '" + testName + "' needs to be a function.");
+
+                        return promise.then(() => {
+                            return Promise.resolve();
+                        });
                     }
 
                     return promise.then(() => {
-                        try {
+                        let result = defaultModules[testName]();
+                        result = result instanceof Promise ? result : Promise.resolve(result);
 
-                            let result = defaultModules[testName]();
-                            result = result instanceof Promise ? result : Promise.resolve(result);
-
-                            return result.then(() => {
-                                console.log("PASSED: " + testName);
-                            }).catch((error) => {
-                                console.log("FAILED: " + testName);
-                                console.log(error);
-                            });
-
-                        } catch (error) {
-
-                            console.log("FAILED: " + testName);
-                            console.log(error);
-
-                            return Promise.resolve(null);
-
-                        }
+                        return result;
                     }).then(() => {
-
+                        console.log("PASSED: " + testName);
+                    }).catch((error) => {
+                        console.log("FAILED: " + testName);
+                        console.log(seperator);
+                        console.log(error);
+                        console.log(seperator);
+                    }).then(() => {
                         return clean();
-
+                    }).catch((error) => {
+                        console.log("UNEXPECTED FAILURE ON CLEAN");
+                        console.log(seperator);
+                        console.log(error);
+                        console.log(seperator);
                     });
-                }, prepare()).catch((error) => {
 
-                    console.log("UNEXPECTED FAILURE: Most likely an async error.");
-
-                }).then(() => {
-
+                }, preparePromise).then(() => {
                     return destroy();
-
+                }).catch((error) => {
+                    console.log("UNEXPECTED FAILURE ON DESTROY");
+                    console.log(seperator);
+                    console.log(error);
+                    console.log(seperator);
+                }).then(() => {
+                    console.log(startSeperator);
+                    console.log("\n\n");
                 });
             }
 
             return Promise.resolve();
+        }).then(() => {
         });
 
     }, Promise.resolve());
-
 }).catch((error) => {
-    console.log("UNEXPECTED FAILURE: " + error.message);
+    console.log(`Couldn't find test folder here: ${testDirectory}`);
 });
